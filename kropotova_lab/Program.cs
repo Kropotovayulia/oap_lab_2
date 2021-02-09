95d7a739609cc67065721692a40ea00cd506a27c
Noirala <73240860+Noirala@users.noreply.github.com>
2021-02-09 11:01:01 +0300

    second

--- kropotova_lab/Program.cs
+++ kropotova_lab/Program.cs
@@ -10,7 +10,7 @@ namespace kropotova_lab
     {
         static void Main(string[] args)
         {
-            // команда Console.Write выводит текст в консоль
+             // команда Console.Write выводит текст в консоль
             //Console.Write("Input katet1: ");
             /* Нахождениме площади прямоугольного треугольника
             // команда Console.ReadLine читает СТРОКУ из консоли
@@ -37,9 +37,9 @@ namespace kropotova_lab
 
             /**/
             // команда Console.Write выводит текст в консоль
-            Console.Write("Input R: ");
-            var R = Console.ReadLine();
-            
+            // Console.Write("Input R: ");
+            // var R = Console.ReadLine();
+
             //Console.Write("Input P: ");
             //var P = Console.ReadLine();
 
@@ -48,12 +48,62 @@ namespace kropotova_lab
             // Math.Pow - возведение в степень
             // Convert.ToDouble - преобразует строку в число
             // var C = 2 * PI* R
-            var Square = Math.PI* Math.Pow(Convert.ToDouble(R), 2);
+            // var Square = Math.PI* Math.Pow(Convert.ToDouble(R), 2);
 
-            Console.WriteLine($"Square = {Square}");
+            //  Console.WriteLine($"Square = {Square}");
 
-            Console.WriteLine("enter");
-            Console.ReadLine();
+            // Console.WriteLine("enter");
+            //  Console.ReadLine();
+
+
+
+
+            // подключены какие-то библиотеки
+
+
+            {
+                // команда Console.Write выводит текст в консоль
+                Console.Write("Input Xa: ");
+                var Xa = Console.ReadLine();
+                Console.Write("Input Ya: ");
+                var Ya = Console.ReadLine();
+                Console.Write("Input Xb: ");
+                var Xb = Console.ReadLine();
+                Console.Write("Input Yb: ");
+                var Yb = Console.ReadLine();
+                Console.Write("Input Xc: ");
+                var Xc = Console.ReadLine();
+                Console.Write("Input Yc: ");
+                var Yc = Console.ReadLine();
+
+                var AB = Math.Sqrt(
+                        Math.Pow(Convert.ToDouble(Xb) - Convert.ToDouble(Xa), 2) +
+                        Math.Pow(Convert.ToDouble(Yb) - Convert.ToDouble(Ya), 2)
+                );
+                var BC = Math.Sqrt(
+                        Math.Pow(Convert.ToDouble(Xc) - Convert.ToDouble(Xb), 2) +
+                        Math.Pow(Convert.ToDouble(Yc) - Convert.ToDouble(Yb), 2)
+                );
+                var AC = Math.Sqrt(
+                      Math.Pow(Convert.ToDouble(Xc) - Convert.ToDouble(Xa), 2) +
+                      Math.Pow(Convert.ToDouble(Yc) - Convert.ToDouble(Ya), 2)
+              );
+                var Perimetr = Convert.ToDouble(AB) + Convert.ToDouble(BC) + Convert.ToDouble(AC);
+
+                var PolyPerimetr = Convert.ToDouble(Perimetr) / 2;
+                var ploshad = Math.Sqrt(
+                    Convert.ToDouble(PolyPerimetr) * (Convert.ToDouble(PolyPerimetr) - Convert.ToDouble(AB)) *
+                    (Convert.ToDouble(PolyPerimetr) - Convert.ToDouble(BC)) *
+                    (Convert.ToDouble(PolyPerimetr) - Convert.ToDouble(AC))
+                    );
+                // выводим результат
+                // знак $ перед строкой указывает, что внутри строки в фигурных скобках названия переменных
+                Console.WriteLine($"Perimetr = {Perimetr}");
+                Console.WriteLine($"ploshad = {ploshad}");
+                // читаем строку, чтобы консольное окно сразу не закрылось
+                Console.Write("Press ENTER to continue...");
+                Console.ReadLine();
+            }
         }
     }
 }
